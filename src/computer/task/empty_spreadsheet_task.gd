f4980b97eb57a97861a16364680c69d4f48c2c68
jelleas <jelle.van.assema@gmail.com>
2026-07-23 20:18:35 +0200

    better empty

--- src/computer/task/empty_spreadsheet_task.gd
+++ src/computer/task/empty_spreadsheet_task.gd
@@ -1,6 +1,9 @@
 class_name EmptySpreadsheetTask
 extends SpreadsheetTask
 
+const MIN_FILLED_CELLS := 5
+const MAX_FILLED_CELLS := 15
+
 func _init(spreadsheet: Spreadsheet) -> void:
 	super._init(
 		"Empty",
@@ -11,9 +14,17 @@ func _init(spreadsheet: Spreadsheet) -> void:
 func start_task() -> void:
 	var spreadsheet := get_spreadsheet()
 	spreadsheet.clear_cells()
-	for row in range(spreadsheet.ROWS):
-		for col in range(spreadsheet.COLS):
-			spreadsheet.set_cell_text(row, col, str(randi_range(1, 99)))
+
+	var filled_cells: Dictionary[Vector2i, bool] = {}
+	var cell_count := randi_range(MIN_FILLED_CELLS, MAX_FILLED_CELLS)
+
+	while filled_cells.size() < cell_count:
+		var cell := Vector2i(randi_range(0, spreadsheet.ROWS - 1), randi_range(0, spreadsheet.COLS - 1))
+		if filled_cells.has(cell):
+			continue
+
+		filled_cells[cell] = true
+		spreadsheet.set_cell_text(cell.x, cell.y, str(randi_range(1, 99)))
 
 func check_completed() -> bool:
 	var spreadsheet := get_spreadsheet()
